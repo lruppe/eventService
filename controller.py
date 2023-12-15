7af96f6ff2ade0ba5b38fbbbfd4c4e94ab66a8aa
lruppe <leon_ruppen@hotmail.com>
2023-12-15 18:41:48 +0100

    - read file once - endpoint with details

--- controller.py
+++ controller.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 import eventService
 
 app = FastAPI()
+
 @app.get("/events", response_model=List[str])
 async def get_events():
     return eventService.get_events()
+
+@app.get("/eventsWithDetails", response_model=List[str])
+async def get_events():
+    return eventService.get_events_with_details()
 
--- eventService.py
+++ eventService.py
@@ -1,11 +1,16 @@
+with open('events_details.txt', 'r', encoding='utf-8') as f:
+    events_with_details = f.readlines()
 
+with open('events.txt', 'r', encoding='utf-8') as f:
+    events = f.readlines()
+
+def get_events_with_details():
+    return events_with_details
 def get_events():
-    with open('events.txt', 'r') as f:
-        events = f.readlines()
     return events
 
 
 if __name__ == '__main__':
-    events = get_events()
-    for event in events:
+    events_with_details = get_events_with_details()
+    for event in events_with_details:
         print(event)
