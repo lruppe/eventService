738a471d8dafd8d1be3d6b6681c9a17203db00d6
lruppe <leon_ruppen@hotmail.com>
2023-12-15 22:10:36 +0100

    - better doc for api

--- controller.py
+++ controller.py
@@ -11,10 +11,28 @@ app = FastAPI()
 
 @app.get("/events", response_model=List[str])
 async def get_events():
+    """
+    Retrieves a list of events
+
+    Args:
+        none
+
+    Returns:
+        A list of events including their basic information.
+    """
     return event_service.get_events()
 
 @app.get("/events/{uuid}", response_model=Event)
 async def get_events_with_details(uuid: uuid.UUID):
+    """
+    Retrieves a detailed description of an event
+
+    Args:
+        item_id (int): The ID of the item to retrieve.
+
+    Returns:
+        An event including its detailed information.
+    """
     event = event_service.get_event_with_details(uuid)
     if event is None:
         raise HTTPException(status_code=404, detail="Event not found")
